4e41f3156d0096c7448ce8e2ee3467e4e427b1a1
MathRaven <157055670+MathRaven@users.noreply.github.com>
2024-04-02 20:35:20 -0400

    Finished

--- Assignments/Assignment-4/Part-2/main.js
+++ Assignments/Assignment-4/Part-2/main.js
@@ -22,8 +22,8 @@ const alts = {
 /* Looping through images */
 for (const image of images) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', xxx);
-    newImage.setAttribute('alt', xxx);
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
     thumbBar.appendChild(newImage);
     newImage.addEventListener('click', e => {
         displayedImage.src = e.target.src;
@@ -31,3 +31,16 @@ for (const image of images) {
     })
 }
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass == 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+})
