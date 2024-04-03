18663d41853cd65be6d83195cf79da9c4ab18591
MathRaven <157055670+MathRaven@users.noreply.github.com>
2024-04-02 20:25:51 -0400

    alts

--- Assignments/Assignment-4/Part-2/main.js
+++ Assignments/Assignment-4/Part-2/main.js
@@ -6,10 +6,20 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg',];
+
+
 /* Declaring the alternative text for each image file */
 
-/* Looping through images */
+const alts = {
+    'pic1.jpg' : 'Some person\'s eye',
+    'pic2.jpg' : 'Pattern thing',
+    'pic3.jpg' : 'Flowers',
+    'pic4.jpg' : 'Ancient Egyptian Art',
+    'pic5.jpg' : 'Big moth'
+}
 
+/* Looping through images */
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
